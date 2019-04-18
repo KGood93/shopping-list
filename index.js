86c8d7b0f95b43fcd3305e901ea7572a61fbc4e4
KA Goodreau <goodreau.kath@gmail.com>
2019-04-18 16:03:52 -0500

    javaScrit is working as described

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//Do these need to be in diffent functions or one parent function
 $(function() {
    //Handle button click from form
     $("#js-shopping-list-form").submit(function(event) {
-       //prevent page from refreshing
-        event.preventDefault();
-       //retrive input to string
-        const userItem = $(event.currentTarget).find("#shopping-list-entry");
-       //add new list item to unordered list
-       //should add whole item including buttons
-        $("ul").append('<li><span class="shopping-item">' + userItem.val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-        //console.log(userItem);
-        userItem.val("");
+      //prevent page from refreshing
+      event.preventDefault();
+      //retrive input to string
+      const userItem = $(event.currentTarget).find("#shopping-list-entry");
+      //add new list item to unordered list
+      $("ul").append('<li>' + '<span class="shopping-item">' + userItem.val() + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+      //console.log(event);
+      userItem.val("");
     });
 
-});
-
-$(function() {
-   //When Check button in pressed add shopping-item_checked to span class
-   $('li').on('click', '.shopping-item-toggle', function(event) {
-        $(event.delegateTarget).addClass('shopping-item__checked');
+   //When Check button in pressed add shopping-item_checked to list item
+   //ul is the element above li and it needed to affect newly added li's
+   $('ul').on('click','li', function(event) {
+      //console.log(event); 
+      //currentTarget is li, delegateTarget is ul
+      $(event.currentTarget).toggleClass('shopping-item__checked');
     });
 
-    $('li').on('click', '.shopping-item-delete', function(event) {
-        //console.log(event);
-       $(event.delegateTarget).remove();
+    //Deletes items when delete button is pressed.
+    //ul is element above li and needed to affect newly added li's
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+      //console.log(event);
+      //this targets the button, reaches up to the parent li using the closest method
+      $(this).closest('li').remove();
     });
 });
